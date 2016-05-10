880533d23ffbd23bef6a47c6a10443fe0a43f3ae
gareth4192 <gareth-123@live.com>
2016-05-10 11:44:59 +0100

    mean stack structure set up

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('blindsSiteApp')
+  .directive('navbar', function () {
+    return {
+      templateUrl: 'components/navbar/navbar.html',
+      restrict: 'E',
+      controller: 'NavbarCtrl'
+    };
+  });
