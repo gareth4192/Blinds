7de3c4c245e495db2ee75f37d0c6270fc157d4f8
gareth4192 <gareth-123@live.com>
2016-05-17 15:42:23 +0100

    added a product filter

--- e2e/products/products.spec.js
+++ e2e/products/products.spec.js
@@ -1,89 +1,172 @@
 'use strict';
 
+var path = require('path');
+var config = browser.params;
+var ProductModel = require(config.serverConfig.root + '/server/api/product/product.model');
+var UserModel = require(config.serverConfig.root + '/server/api/user/user.model');
+var page = require('../account/login/login.po');
+var navbar = require('../components/navbar/navbar.po');
+var user = {
+  provider: 'local',
+  name: 'Test User',
+  email: 'test@test.com',
+  password: 'test'
+};
+var admin = {
+  provider: 'local',
+  role: 'admin',
+  name: 'Admin',
+  email: 'admin@admin.com',
+  password: 'admin'
+};
+
 describe('Products View', function() {
-  var page = require('./product.po'),
+  var product = require('./product.po'),
       timestamp = (new Date()).getTime(),
       title = 'Product ' + timestamp,
       description = title + ' description',
-      price = parseInt(timestamp / 100000000000);
+      price = parseInt(timestamp / 100000000000),
+      filePath = '../fixtures/meanbook.jpg',
+      absolutePath = path.resolve(__dirname, filePath),
+      newFilePath = '../fixtures/meanstack.png',
+      newAbsPath = path.resolve(__dirname, newFilePath);
+
+  before(function () {
+    return ProductModel.removeAsync().then(function () {
+      return UserModel.removeAsync();
+    }).then(function(){
+      return UserModel.createAsync(user, admin);
+    });
+  });
+
+  after(function () {
+    return ProductModel.removeAsync().then(function () {
+      return UserModel.removeAsync();
+    });
+  });
+
+  describe('Authentication', function() {
+    beforeEach(function () {
+      browser.get(config.baseUrl + '/login');
+    });
+
+    it('should NOT create a product with a non-authenticated user', function() {
+      browser.get(config.baseUrl + '/products/new');
+      expect(browser.getCurrentUrl()).to.eventually.equal(config.baseUrl + '/admin-access');
+    });
+
+    it('should NOT create a product with a non-admin user', function() {
+      page.login(user);
+      navbar.navbarAccountGreeting.click();
+      browser.get(config.baseUrl + '/products/new');
+      expect(browser.getCurrentUrl()).to.not.eventually.match(/\/products\/new$/);
+    });
+
+    it('should access a product with an admin user', function() {
+      page.login(admin);
+      navbar.navbarAccountGreeting.click();
+      navbar.createProduct.click();
+      expect(browser.getCurrentUrl()).to.eventually.match(/\/products\/new$/);
+    });
+  });
 
   describe('CREATE Products', function() {
+
     beforeEach(function () {
-      browser.get('/');
+      browser.get(config.baseUrl + '/login');
+      page.login(admin);
       // link to create product
-      element(by.buttonText('Add Product')).click();
+      navbar.navbarAccountGreeting.click();
+      navbar.createProduct.click();
       expect(browser.getCurrentUrl()).to.eventually.match(/\/products\/new$/);
 
       // filling out the form
-      element(by.model('product.title')).sendKeys(title);
-      element(by.model('product.description')).sendKeys(description);
+      product.inputTitle.sendKeys(title);
+      product.inputDescription.sendKeys(description);
     });
 
     it('should create a product', function() {
+      product.inputFile.sendKeys(absolutePath);
       element(by.model('product.price')).sendKeys(price);
-      page.saveButton.click();
+      product.saveButton.click().then(function () {
+        // should redirect to product view
+        expect(browser.getCurrentUrl()).to.eventually.match(/view$/);
 
-      // should redirecto to product page
-      expect(browser.getCurrentUrl()).to.eventually.match(/\/products\//);
-
-      // should have fields
-      expect(page.title.getText()).to.eventually.equal(title);
-      expect(page.description.getText()).to.eventually.equal(description);
-      expect(page.price.getText()).to.eventually.equal('$' + price.toFixed(2));
+        // should have fields
+        expect(product.title.getText()).to.eventually.equal(title);
+        expect(product.description.getText()).to.eventually.equal(description);
+        expect(product.price.getText()).to.eventually.equal('$' + price.toFixed(2));
+      });
     });
 
     it('should show an error if price is not provided', function() {
-      page.saveButton.click();
+      product.inputFile.sendKeys(absolutePath);
+      product.saveButton.click();
       expect(element(by.className('errors')).getText()).to.eventually.match(/`price` is required/);
+      expect(browser.getCurrentUrl()).to.eventually.match(/\/products\/new$/);
+    });
+
+    it('should show an error if picture is not uploaded', function() {
+      product.inputTitle.sendKeys('no pic');
+      element(by.model('product.price')).sendKeys(price);
+      product.saveButton.click();
+      expect(element(by.className('errors')).getText()).to.eventually.match(/file/);
       expect(browser.getCurrentUrl()).to.eventually.match(/\/products\/new$/);
     });
   });
 
   describe('READ Products', function() {
     beforeEach(function () {
-      browser.get('/products');
+      browser.get(config.baseUrl + '/login');
+      page.login(admin);
+      navbar.menuItem('Products').click();
     });
 
     it('should have the newly created product', function() {
-      expect(element.all(page.products.column("product.title")).first().getText()).to.eventually.equal(title);
-      expect(element.all(page.products.column("product.price")).first().getText()).to.eventually.equal('$' + price.toFixed(2));
+      expect(element.all(product.products.column("product.title")).getText()).to.eventually.contain(title);
+      expect(element.all(product.products.column("product.price")).getText()).to.eventually.contain('$' + price.toFixed(2));
     });
 
-    it('should truncate long descriptions to 100 chars', function() {
-      expect(element.all(page.products.column("product.description")).first().getText())
-        .to.eventually.equal(description.substring(0, 100) + ' ...');
-    });
   });
 
   describe('UPDATE products', function() {
     beforeEach(function () {
-      browser.get('/products');
-      expect(element.all(page.products).count()).to.eventually.equal(1);
-      page.detailsLink.click();
-      element(by.linkText('Edit')).click();
-      expect(browser.getCurrentUrl()).to.eventually.match(/edit$/);
+      browser.get(config.baseUrl + '/login');
+      page.login(admin);
+      navbar.menuItem('Products').click();
     });
 
-    it('should update the title', function() {
-      page.inputTitle.sendKeys('Updated');
-      page.inputDescription.sendKeys('Updated');
-      page.inputPrice.sendKeys('.12');
-      page.saveButton.click();
-      expect(browser.getCurrentUrl()).not.to.eventually.match(/edit$/);
-      expect(page.title.getText()).to.eventually.equal(title + 'Updated');
-      expect(page.description.getText()).to.eventually.equal(description + 'Updated');
-      expect(page.price.getText()).to.eventually.equal('$' + (price + 0.12).toFixed(2));
+    it('should update the title, description, price and image', function() {
+      element(by.linkText(title)).click();
+      element(by.linkText('EDIT')).click();
+      expect(browser.getCurrentUrl()).to.eventually.match(/edit$/);
+
+      product.inputTitle.sendKeys('Updated');
+      product.inputDescription.sendKeys('Updated');
+      product.inputPrice.sendKeys('.12');
+      product.inputFile.sendKeys(newAbsPath);
+      product.saveButton.click().then(function () {
+        expect(browser.getCurrentUrl()).not.to.eventually.match(/edit$/);
+        expect(product.title.getText()).to.eventually.equal(title + 'Updated');
+        expect(product.description.getText()).to.eventually.equal(description + 'Updated');
+        expect(product.price.getText()).to.eventually.equal('$' + (price + 0.12).toFixed(2));
+        expect(product.imageSrc.getAttribute('src')).to.eventually.match(/png$/);
+      });
     });
   });
 
   describe('DELETE products', function () {
+    beforeEach(function () {
+      browser.get(config.baseUrl + '/login');
+      page.login(admin);
+      navbar.menuItem('Products').click();
+    });
+
     it('should be able to delete existing product', function() {
-      browser.get('/products');
-      expect(element.all(page.products).count()).to.eventually.equal(1);
-      page.detailsLink.click();
-      element(by.linkText('Delete')).click();
+      element(by.linkText(title + 'Updated')).click();
+      element(by.linkText('DELETE')).click();
       expect(browser.getCurrentUrl()).to.eventually.match(/\/products$/);
-      expect(element.all(page.products).count()).to.eventually.equal(0);
+      expect(element.all(product.products.column("product.title")).getText()).not.to.eventually.contain(title + 'Updated');
     });
   });
 });
--- server/config/seed.js
+++ server/config/seed.js
@@ -5,68 +5,10 @@
 
 'use strict';
 
-var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
-
 var Product = require('../api/product/product.model');
-   Product.find({}).removeAsync()
-     .then(function() {
-       Product.createAsync({
-         title: 'Blind 1',
-         imageUrl: '/assets/uploads/b1.jpg',
-         price: 25,
-         stock: 250,
-         description: 'Vertical'
-        }, {
-        title: 'Blind 2',
-        imageUrl: '/assets/uploads/b2.jpg',
-        price: 15,
-        stock: 100,
-        description: 'Roller'
-        }, {
-        title: 'Blind 4',
-        imageUrl: '/assets/uploads/b3.jpg',
-        price: 8,
-        stock: 50,
-        description: 'Shutter'
-       })
-       .then(function() {
-         console.log('finished populating products');
-       });
-});
-
-
-Thing.find({}).removeAsync()
-  .then(function() {
-    Thing.create({
-      name: 'Development Tools',
-      info: 'Integration with popular tools such as Bower, Grunt, Karma, ' +
-             'Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, ' +
-             'Stylus, Sass, CoffeeScript, and Less.'
-    }, {
-      name: 'Server and Client integration',
-      info: 'Built with a powerful and fun stack: MongoDB, Express, ' +
-             'AngularJS, and Node.'
-    }, {
-      name: 'Smart Build System',
-      info: 'Build system ignores `spec` files, allowing you to keep ' +
-             'tests alongside code. Automatic injection of scripts and ' +
-             'styles into your index.html'
-    }, {
-      name: 'Modular Structure',
-      info: 'Best practice client and server structures allow for more ' +
-             'code reusability and maximum scalability'
-    }, {
-      name: 'Optimized Build',
-      info: 'Build process packs up your templates as a single JavaScript ' +
-             'payload, minifies your scripts/css/images, and rewrites asset ' +
-             'names for caching.'
-    }, {
-      name: 'Deployment Ready',
-      info: 'Easily deploy your app to Heroku or Openshift with the heroku ' +
-             'and openshift subgenerators'
-    });
-  });
+var Catalog = require('../api/catalog/catalog.model');
+var mainCatalog, vertical, roller, shutter;
 
 User.find({}).removeAsync()
   .then(function() {
@@ -80,9 +22,62 @@ User.find({}).removeAsync()
       role: 'admin',
       name: 'Admin',
       email: 'admin@admin.com',
-      password: 'admin'
+      password: process.env.ADMIN_PASSWORD || 'admin'
     })
     .then(function() {
       console.log('finished populating users');
     });
+  });
+
+Catalog
+  .find({})
+  .remove()
+  .then(function () {
+    return Catalog.create({ name: 'All'});
+  })
+  .then(function (catalog) {
+    mainCatalog = catalog;
+    return mainCatalog.addChild({name: 'Vertical'});
+  })
+  .then(function (category) {
+    vertical = category._id;
+    return mainCatalog.addChild({name: 'Roller'});
+  })
+  .then(function (category) {
+    roller = category._id;
+    return mainCatalog.addChild({name: 'Shutter'});
+  })
+  .then(function (category) {
+    shutter = category._id;
+    return Product.find({}).remove({});
+  })
+  .then(function() {
+    return Product.create({
+      title: 'Blue fabric vertical blind',
+      imageUrl: '/assets/uploads/b1.jpg',
+      price: 25,
+      stock: 250,
+      categories: [vertical],
+      description: 'Vertical'
+    }, {
+      title: 'Brown fabric roller blind',
+      imageUrl: '/assets/uploads/b2.jpg',
+      price: 15,
+      stock: 100,
+      categories: [roller],
+      description: 'Roller'
+    }, {
+      title: 'White veneer shutter blind',
+      imageUrl: '/assets/uploads/b3.jpg',
+      price: 8,
+      stock: 50,
+      categories: [shutter],
+      description: 'Shutter'
+    });
+  })
+  .then(function () {
+    console.log('Finished populating Products with categories');
+  })
+  .then(null, function (err) {
+    console.error('Error populating Products & categories: ', err);
   });
